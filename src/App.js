ddb2f1dd78b3e5388031b8b93ad37d0f4d1cc31a
Ke Wang <453587854@qq.com>
2018-04-28 01:36:51 +0800

    detect caret position change

--- src/App.js
+++ src/App.js
@@ -3,25 +3,52 @@ import cronstrue from 'cronstrue';
 
 class App extends Component {
   state = { value: "* * * * *", explanation: "", isValid: true };
+  inputRef;
+
+  lastCaretPosition = 0;
+
+  onCaretPositionChange() {
+    if (!this.inputRef) {
+      return;
+    }
+    let caretPosition = this.inputRef.selectionStart;
+    if (this.lastCaretPosition === caretPosition) {
+      return;
+    }
+    this.lastCaretPosition = caretPosition;
+    console.log("caret pos", caretPosition);
+  }
 
   render() {
     return (
       <div>
-        <input type="text" className="cron-input" value={this.state.value} onChange={e => {
-          let explanation;
-          let isValid = true;
-          try {
-            explanation = cronstrue.toString(e.target.value);
-          } catch (e) {
-            explanation = e.toString();
-            isValid = false;
-          }
-          this.setState({
-            value: e.target.value,
-            explanation: explanation,
-            isValid,
-          })
-        }}/>
+        <input type="text" className="cron-input"
+               value={this.state.value}
+               ref={ref => {
+                 this.inputRef = ref;
+               }}
+               onMouseUp={e => {
+                 this.onCaretPositionChange()
+               }}
+               onKeyUp={e => {
+                 this.onCaretPositionChange()
+               }}
+               onChange={e => {
+                 let explanation;
+                 let isValid = true;
+                 try {
+                   explanation = cronstrue.toString(e.target.value);
+                 } catch (e) {
+                   explanation = e.toString();
+                   isValid = false;
+                 }
+                 this.setState({
+                   value: e.target.value,
+                   explanation: explanation,
+                   isValid,
+                 });
+                 this.onCaretPositionChange()
+               }}/>
 
         <div>
           {this.state.explanation}
