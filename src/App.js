64ceddd6e09c28bb550afc73c088cbbaecbbf01e
Ke Wang <453587854@qq.com>
2018-04-28 01:27:14 +0800

    use sass

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 import cronstrue from 'cronstrue';
 
-console.log(cronstrue.toString("0 23 ? * MON-FRI"));
-
 class App extends Component {
-  state = { value: "", explanation: "", isValid: true };
+  state = { value: "* * * * *", explanation: "", isValid: true };
 
   render() {
     return (
       <div>
-        <input type="text" value={this.state.value} onChange={e => {
+        <input type="text" className="cron-input" value={this.state.value} onChange={e => {
           let explanation;
           let isValid = true;
           try {
